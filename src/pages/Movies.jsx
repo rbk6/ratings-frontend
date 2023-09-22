590112c52a7345a1df62d64d8c0245d603ddfe88
rbk6 <rmbackman@gmail.com>
2023-09-22 03:10:44 -0700

    implemented getMoviePreviews to update accessToken and get preview data from the server to be passed into Preview component

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,9 +1,66 @@
-const Movies = () => {
+import PropTypes from 'prop-types'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import Preview from '../components/Preview'
+
+const Movies = ({ logoutMsg }) => {
+  const apiUrl = import.meta.env.VITE_API_URL
+  let page = 1
+  const [moviePreviews, setMoviePreviews] = useState({ data: { results: [] } })
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const handleLogout = () => {
+      sessionStorage.removeItem('rate')
+      logoutMsg('Your session has expired, you have been logged out.')
+      navigate('/login')
+    }
+
+    const getMoviePreviews = async () => {
+      try {
+        const res = await axios.get(`${apiUrl}/movies/${page}`, {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem('rate')}`,
+          },
+        })
+
+        if (res.data.accessToken) {
+          console.log('updating token')
+          const updatedToken = res.data.accessToken
+          sessionStorage.setItem('rate', updatedToken)
+          const updatedRes = await axios.get(`${apiUrl}/movies/${page}`, {
+            headers: {
+              Authorization: `Bearer ${updatedToken}`,
+            },
+          })
+          setMoviePreviews(updatedRes.data)
+        } else {
+          setMoviePreviews(res.data)
+        }
+      } catch (err) {
+        if (err.message.includes('403')) handleLogout()
+        else console.log('Error fetching movies: ', err)
+      }
+    }
+
+    getMoviePreviews()
+  }, [apiUrl, logoutMsg, navigate, page])
+
   return (
     <>
-      <div>Start browsing movies!</div>
+      {moviePreviews.data.results ? (
+        <Preview previews={moviePreviews.data.results} />
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   )
+}
+
+Movies.propTypes = {
+  logoutMsg: PropTypes.func,
 }
 
 export default Movies
