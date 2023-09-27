a44c699ad3a6831ea604595efafe1b48abd2e633
rbk6 <rmbackman@gmail.com>
2023-09-26 18:45:51 -0700

    updated Preview component layout/styling and added overview toggle for content description

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,7 +1,17 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 import '../styles/prev.css'
 
 const Preview = ({ previews }) => {
+  const [expandOverview, setExpandOverview] = useState([])
+
+  const toggleOverview = (id) => {
+    setExpandOverview((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id],
+    }))
+  }
+
   return (
     <div className="prev-container">
       <div className="preview-wrapper">
@@ -14,18 +24,62 @@ const Preview = ({ previews }) => {
           const formattedDate = `${month}-${day}-${year}`
 
           let overview = preview.overview
-          if (overview.length > 200) {
-            overview = overview.slice(0, 200).trimEnd() + '...'
+          if (overview.length > 70 && !expandOverview[preview.id]) {
+            overview = overview.slice(0, 70).trimEnd()
+            let lastChar = overview.charAt(overview.length - 1)
+            if (['.', '!', '?', ','].includes(lastChar)) {
+              overview = overview.slice(0, -1)
+            }
+
+            overview = overview + '...'
           }
           return (
             <div className="preview" key={preview.id} id={preview.id}>
               <img
                 src={`https://image.tmdb.org/t/p/w500/${preview.poster_path}`}
-              ></img>
+                alt={`poster image for ${preview.original_title}`}
+              />
               <div className="preview-info">
-                <h2>{preview.original_title}</h2>
-                <h3>{formattedDate}</h3>
-                <p>{overview}</p>
+                <h3>{preview.original_title}</h3>
+                <h4>Released {formattedDate}</h4>
+                <p>
+                  {overview}
+                  {preview.overview.length > 70 && (
+                    <span>
+                      {' '}
+                      <a
+                        href="#"
+                        className="prev-link"
+                        onClick={(e) => {
+                          e.preventDefault()
+                          toggleOverview(preview.id)
+                        }}
+                      >
+                        {expandOverview[preview.id] ? 'Show Less' : 'Read More'}
+                      </a>
+                    </span>
+                  )}
+                </p>
+                <div className="btn-container">
+                  <button
+                    style={{
+                      borderBottomLeftRadius: '12px',
+                      borderTopRightRadius: '12px',
+                    }}
+                    className="prev-btn"
+                  >
+                    Add to List
+                  </button>
+                  <button
+                    style={{
+                      borderBottomRightRadius: '12px',
+                      borderTopLeftRadius: '12px',
+                    }}
+                    className="prev-btn"
+                  >
+                    Create Rating
+                  </button>
+                </div>
               </div>
             </div>
           )
