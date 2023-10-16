d5fd5c3ad36eb64ec7ddcb014022858c91d5be5a
rbk6 <rmbackman@gmail.com>
2023-10-15 17:37:58 -0700

    updated sizing and position of CircularProgress element, removed loading text

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,35 +1,22 @@
-import { useState, useEffect } from 'react'
 import { CircularProgress } from '@mui/material'
 
-const LoadingComponent = () => {
-  const [dots, setDots] = useState('Loading Content')
-
-  useEffect(() => {
-    const updateDots = () => {
-      setDots((prevDots) => {
-        if (prevDots === 'Loading Content...') return 'Loading Content'
-        else return prevDots + '.'
-      })
-    }
-
-    const dotsTimer = setInterval(updateDots, 1000)
-    return () => clearInterval(dotsTimer)
-  }, [])
-
+const Loading = () => {
   return (
     <div
       style={{
-        fontSize: '24px',
-        textAlign: 'center',
         display: 'flex',
-        flexDirection: 'column',
+        justifyContent: 'center',
         alignItems: 'center',
+        width: '100%',
+        height: '80vh',
       }}
     >
-      {dots}
-      <CircularProgress style={{ color: '#6020dd', marginTop: '24px' }} />
+      <CircularProgress
+        size={'5rem'}
+        style={{ color: '#6020dd', marginTop: '24px' }}
+      />
     </div>
   )
 }
 
-export default LoadingComponent
+export default Loading
